48bf7ba8dce2558e08c86c0583b71a830b5516a8
Cody Brown <cody6lefever@gmail.com>
2020-02-14 11:54:58 -0800

    tests pass

--- src/PokeItem.test.js
+++ src/PokeItem.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<PokeItem />)
+    .create(<PokeItem pokemon={[]}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
